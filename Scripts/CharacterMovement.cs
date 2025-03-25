ca425b7e0345bb59f9e935ef469bc3649bd06bf1
Jacob Neilands <jn908@hotmail.com>
2025-03-25 19:41:27 +1100

    Movement and Sprint now work
    Movement and Sprint now work correctly

--- Scripts/CharacterMovement.cs
+++ Scripts/CharacterMovement.cs
@@ -1,7 +1,24 @@
 using Godot;
 using System;
 
-public partial class CharacterMovement : Node
+public partial class CharacterMovement : CharacterBody2D
 {
-    
+	[Export]
+	public int Speed { get; set; } = 400;
+	[Export]
+	public int SprintSpeed { get; set; } = 600;
+	public void GetInput()
+	{
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		if (Input.IsActionPressed("sprint")) {
+			Velocity = inputDirection * SprintSpeed;
+		} else {
+			Velocity = inputDirection * Speed;
+		}
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
 }
